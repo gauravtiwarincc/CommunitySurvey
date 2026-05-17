4ea53cc41067bb621814a1a8bc05fe4a9989fd2a
Gaurav Tiwari <gauravtiwarinccc@gmail.com>
2026-05-17 14:42:35 +0530

    Initial Commit

---
+++ CommunitySurvey/CommunitySurveyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommunitySurveyApp.swift
+//  CommunitySurvey
+//
+//  Created by Gaurav Tiwari on 17/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CommunitySurveyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
